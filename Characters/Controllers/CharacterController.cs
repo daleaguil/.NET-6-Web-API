cfe002cd7e98b45d9189308b4146c2397ec58b87
Dale Aguil <dale.aguil@mobilemoney.ph>
2022-05-16 16:51:02 +0800

    Add project files.

---
+++ Characters/Controllers/CharacterController.cs
@@ -0,0 +1,99 @@
+﻿using Characters.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Characters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private static List<Character> characters = new List<Character>
+        {
+            new Character
+            {
+                Id = 1,
+                FirstName = "Tony",
+                LastName = "Stark",
+                Age = 40
+            },
+            new Character
+            {
+                Id = 2,
+                FirstName = "Killua",
+                LastName = "Zoldyck",
+                Age = 12
+            },
+            new Character
+            {
+                Id = 3,
+                FirstName = "Tanjiro",
+                LastName = "Kamado",
+                Age = 13
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> GetCharacters()
+        {
+            return Ok(await _context.Characters.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Character>> GetCharacter(int id)
+        {
+            var character = await _context.Characters.FindAsync(id);
+
+            if (character == null)
+                return BadRequest("Character not found.");
+
+            return Ok(character);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> CreateCharacter(Character character)
+        {
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Characters.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Character>>> UpdateCharacter(Character request)
+        {
+            var character = await _context.Characters.FindAsync(request.Id);
+
+            if (character == null)
+                return BadRequest("Character not found.");
+
+            character.FirstName = request.FirstName;
+            character.LastName = request.LastName;
+            character.Age = request.Age;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Characters.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Character>>> DeleteCharacter(int id)
+        {
+            var character = await _context.Characters.FindAsync(id);
+
+            if (character == null)
+                return BadRequest("Character not found.");
+
+            _context.Characters.Remove(character);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Characters.ToListAsync());
+        }
+    }
+}
---
+++ Characters/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Characters.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Characters.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    
+        public DbSet<Character> Characters { get; set; }
+    }
+}
